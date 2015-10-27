f0b491edaef21674938f0ef667c6ac7d38612a19
pinkyTseng <charlie761216@msn.com>
2015-10-28 01:15:46 +0800

    測試名稱: buyFirst_1 失敗

---
+++ TddHw2/TddHw2/PotterShoppingCart.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TddHw2
+{
+    public class PotterShoppingCart
+    {
+        public int calculateFee()
+        {
+            return 1;
+        }
+    }
+}
---
+++ TddHw2/TddHw2Tests/PotterShoppingCartTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TddHw2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TddHw2.Tests
+{
+    [TestClass()]
+    public class PotterShoppingCartTests
+    {
+        [TestMethod()]
+        public void buyFirst_1()
+        {
+            PotterShoppingCart target = new PotterShoppingCart();
+            int[] first = new int[] { 1, 0, 0, 0, 0 };
+            int expected = 100;
+            int actual = target.calculateFee();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
