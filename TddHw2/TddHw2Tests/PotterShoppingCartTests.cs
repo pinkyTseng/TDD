000e493100f4f585c5ee3404d09e0b4322202aa7
pinkyTseng <charlie761216@msn.com>
2015-10-28 03:03:34 +0800

    Success buyFirst_1_Second_1_Third_1_Fourth_1_Five_1

--- TddHw2/TddHw2Tests/PotterShoppingCartTests.cs
+++ TddHw2/TddHw2Tests/PotterShoppingCartTests.cs
@@ -50,5 +50,15 @@ namespace TddHw2.Tests
             int actual = target.calculateFee(first);
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod()]
+        public void buyFirst_1_Second_1_Third_1_Fourth_1_Five_1()
+        {
+            PotterShoppingCart target = new PotterShoppingCart();
+            int[] first = new int[] { 1, 1, 1, 1, 1 };
+            int expected = 375;
+            int actual = target.calculateFee(first);
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
