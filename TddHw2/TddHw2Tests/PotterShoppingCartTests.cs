b06b7ab811e4a02ec2a631a24ff8d8caca03aa2f
pinkyTseng <charlie761216@msn.com>
2015-10-30 01:33:36 +0800

    refactor array to List<Book> buyFirst_1_Second_1_Third_1_Fourth_1

--- TddHw2/TddHw2Tests/PotterShoppingCartTests.cs
+++ TddHw2/TddHw2Tests/PotterShoppingCartTests.cs
@@ -57,7 +57,10 @@ namespace TddHw2.Tests
         {
             PotterShoppingCart target = new PotterShoppingCart();
             List<Book> first = new List<Book>{
-                    new Book {name =  "Harry Potter1", Cost= 100, count = 1}
+                    new Book {name =  "Harry Potter1", Cost= 100, count = 1},
+                    new Book {name =  "Harry Potter2", Cost= 100, count = 1},
+                    new Book {name =  "Harry Potter3", Cost= 100, count = 1},
+                    new Book {name =  "Harry Potter4", Cost= 100, count = 1}
                 };
             int expected = 320;
             int actual = target.calculateFee(first);
